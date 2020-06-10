5c0c23b537233a4895b871b467820de186221bd1
MehboobZeirdi <58353919+MehboobZeirdi@users.noreply.github.com>
2020-06-10 13:45:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -56,11 +56,11 @@ def predict_api():
     return jsonify(output)
 
 
-# if __name__ == '__main__':
-#     app.run(host='0.0.0.0', port=8080, debug=True)
-
-
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=8080, debug=True)
+
+
+# if __name__ == '__main__':
+#     app.run(debug=True)
 
 
